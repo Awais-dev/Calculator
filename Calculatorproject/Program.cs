dc7adc438a6ee0716d04ad7ce3c0276154accd3b
Awais <Awaishussain@etech.net>
2019-07-16 13:26:08 +0100

    if statements added

--- Calculatorproject/Program.cs
+++ Calculatorproject/Program.cs
@@ -3,48 +3,99 @@
 namespace Calculatorproject
 {
     class Program
-    {    
+    {
         public static void Main(string[] args)
         {
-            double firstnum;
-            double secondnum;
-            double answer;
-            string ops;
-
             Console.WriteLine("\t\t\tCalculator Project\r");
 
-            Console.WriteLine("Enter First Number\n" );
-            firstnum = double.Parse(Console.ReadLine());
+            var firstnum = 0M;
+            string enteredOperator = string.Empty;
 
-            Console.WriteLine("Select an operator\n");
-            ops = Console.ReadLine();
+            bool firstNumberValid = false;
+            while (!firstNumberValid)
+            {
+                Console.WriteLine("Enter First Number\n");
+                string input = Console.ReadLine();
+                if (decimal.TryParse(input, out firstnum))
+                {
+                    firstNumberValid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Not a number");
+                }
+            }
 
-            Console.WriteLine("Enter Second Number\n");
-            secondnum = double.Parse(Console.ReadLine());
+            bool operatorValid = false;
+            while (!operatorValid)
+            {
+                Console.WriteLine("Select an operator\n");
+                enteredOperator = Console.ReadLine();
+                if (enteredOperator == "+")
+                {
+                    operatorValid = true;
+                }
+                else if (enteredOperator == "-")
+                {
+                    operatorValid = true;
+                }
 
-            if (ops == "+")
+                else if (enteredOperator == "*")
+                {
+                    operatorValid = true;
+                }
+
+                else if (enteredOperator == "/")
+                {
+                    operatorValid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Not an Operator");
+                }
+
+            }
+
+            var secondnum = 0M;
+            
+            bool secondnumValid = false;
+            while (!secondnumValid)
+            {
+                Console.WriteLine("Enter Second Number\n");
+                string input = Console.ReadLine();
+                if (decimal.TryParse(input, out secondnum))
+                {
+                    secondnumValid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Not a number");
+                }
+            }
+
+            decimal answer;
+
+            if (enteredOperator == "+")
             {
                 answer = firstnum + secondnum;
                 Console.Write(answer);
             }
-
-            if (ops == "-")
+            if (enteredOperator == "-")
             {
                 answer = firstnum - secondnum;
                 Console.Write(answer);
             }
-
-            if (ops == "*")
+            if (enteredOperator == "*")
             {
                 answer = firstnum * secondnum;
                 Console.Write(answer);
             }
-
-            if (ops == "/")
+            if (enteredOperator == "/")
             {
                 answer = firstnum / secondnum;
                 Console.Write(answer);
             }
+
 
             Console.ReadKey();
 
