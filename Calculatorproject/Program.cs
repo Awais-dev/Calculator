b24ce90a46a80f7255be8090fc5cad697d0f25d3
Awais Hussain <awaishussain@etech.net>
2019-07-16 10:27:40 +0100

    created calculator

---
+++ Calculatorproject/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Calculatorproject
+{
+    class Program
+    {    
+        public static void Main(string[] args)
+        {
+            double firstnum;
+            double secondnum;
+            double answer;
+            string ops;
+
+            Console.WriteLine("\t\t\tCalculator Project\r");
+
+            Console.WriteLine("Enter First Number\n" );
+            firstnum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Select an operator\n");
+            ops = Console.ReadLine();
+
+            Console.WriteLine("Enter Second Number\n");
+            secondnum = double.Parse(Console.ReadLine());
+
+            if (ops == "+")
+            {
+                answer = firstnum + secondnum;
+                Console.Write(answer);
+            }
+
+            if (ops == "-")
+            {
+                answer = firstnum - secondnum;
+                Console.Write(answer);
+            }
+
+            if (ops == "*")
+            {
+                answer = firstnum * secondnum;
+                Console.Write(answer);
+            }
+
+            if (ops == "/")
+            {
+                answer = firstnum / secondnum;
+                Console.Write(answer);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
